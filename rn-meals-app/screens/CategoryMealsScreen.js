f29ea032752220ce1f17704fe559dd3edead3421
pitt500 <pedro.rojaslo50@gmail.com>
2019-11-12 11:58:02 -0700

    Pass data through Category Meals screen

--- rn-meals-app/screens/CategoryMealsScreen.js
+++ rn-meals-app/screens/CategoryMealsScreen.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import { CATEGORIES } from '../data/dummy-data';
 
 const CategoryMealScreen = props => {
+    const catId = props.navigation.getParam('categoryId');
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
     return (
         <View style={styles.screen}>
             <Text>The category meal screen!</Text>
+            <Text>{selectedCategory.title}</Text>
             <Button title="Go to detail!" onPress={() => {
                 props.navigation.navigate({routeName: 'MealDetail'}) // or props.navigation.navigate('CategoryMeals')
              }} />
