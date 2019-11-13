a5078017329306cd193c862a85647cf1fec7dff2
pitt500 <pedro.rojaslo50@gmail.com>
2019-11-13 12:38:48 -0700

    Fix shadows for iOS

--- rn-meals-app/components/CategoryGridTile.js
+++ rn-meals-app/components/CategoryGridTile.js
@@ -28,7 +28,10 @@ const styles = StyleSheet.create({
         margin: 15,
         height: 150,
         borderRadius: 10,
-        overflow: 'hidden'
+        overflow: Platform.OS === 'android' && Platform.Version >= 21 ? 'hidden': 'visible',
+
+        //Only for Android:
+        elevation: 5
     },
     title: {
         fontFamily: 'open-sans-bold',
@@ -47,9 +50,6 @@ const styles = StyleSheet.create({
         shadowOpacity: 0.26,
         shadowOffset: { width: 0, height: 2 },
         shadowRadius: 10,
-
-        //Only for Android:
-        elevation: 3
     }
 });
 
