8fc7934d58514a7b3f836ebff79d6850ec84cb71
pitt500 <pedro.rojaslo50@gmail.com>
2019-11-13 12:47:31 -0700

    Add Tab bar to include favorites screen

--- rn-meals-app/navigation/MealsNavigator.js
+++ rn-meals-app/navigation/MealsNavigator.js
@@ -1,10 +1,13 @@
 import { createStackNavigator } from 'react-navigation-stack';
 import { createAppContainer } from 'react-navigation';
+import { createBottomTabsNavigator, createBottomTabNavigator } from 'react-navigation-tabs';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import { Platform } from 'react-native';
 
 import CategoriesScreen from '../screens/CategoriesScreen';
 import CategoryMealsScreen from '../screens/CategoryMealsScreen';
 import MealDetailScreen from '../screens/MealDetailScreen';
+import Favorites from '../screens/FavoritesScreen';
 import Colors from '../constants/Colors';
 
 const MealsNavigator = createStackNavigator({
@@ -24,4 +27,9 @@ const MealsNavigator = createStackNavigator({
     }
 });
 
-export default createAppContainer(MealsNavigator);
+const MealFavTabNavigator = createBottomTabNavigator({
+    Meals: MealsNavigator,
+    Favorites: Favorites
+});
+
+export default createAppContainer(MealFavTabNavigator);
