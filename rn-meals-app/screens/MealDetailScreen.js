df2bf0c8229799bd39a5250889a3bd3e71c5bc1d
pitt500 <pedro.rojaslo50@gmail.com>
2019-11-13 12:34:18 -0700

    Add custom header button to right

--- rn-meals-app/screens/MealDetailScreen.js
+++ rn-meals-app/screens/MealDetailScreen.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 
 import { MEALS } from '../data/dummy-data';
+import HeaderButton from '../components/HeaderButton';
 
 const MealDetailScreen = props => {
 
@@ -24,6 +26,17 @@ MealDetailScreen.navigationOptions = (navigationData) => {
 
     return {
         headerTitle: selectedMeal.title,
+        headerRight: (
+            <HeaderButtons HeaderButtonComponent={HeaderButton}>
+                <Item 
+                    title='Favorite' 
+                    iconName='ios-star'
+                    onPress={() => {
+                        console.log('Mark as favorite!');
+                    }}
+                />
+            </HeaderButtons>
+        )
     };
 };
 
