ea74aa0e865e84b644eb3019e0058694e008590b
pitt500 <pedro.rojaslo50@gmail.com>
2019-11-14 15:31:02 -0700

    Remove import MEALS and get data from Redux state

--- rn-meals-app/screens/MealDetailScreen.js
+++ rn-meals-app/screens/MealDetailScreen.js
@@ -1,15 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
     ScrollView,
     View,
     Text,
-    Button,
     StyleSheet,
     Image
 } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+import { useSelector } from 'react-redux';
 
-import { MEALS } from '../data/dummy-data';
 import HeaderButton from '../components/HeaderButton';
 import DefaultText from '../components/DefaultText';
 
@@ -23,8 +22,9 @@ const ListItem = props => {
 
 const MealDetailScreen = props => {
 
+    const availableMeals = useSelector(state => state.meals.meals);
     const mealId = props.navigation.getParam('mealId');
-    const selectedMeal = MEALS.find(meal => meal.id === mealId);
+    const selectedMeal = availableMeals.find(meal => meal.id === mealId);
 
     return (
         <ScrollView>
@@ -48,11 +48,10 @@ const MealDetailScreen = props => {
 };
 
 MealDetailScreen.navigationOptions = (navigationData) => {
-    const mealId = navigationData.navigation.getParam('mealId');
-    const selectedMeal = MEALS.find(meal => meal.id === mealId);
+    const mealTitle = navigationData.navigation.getParam('mealTitle');
 
     return {
-        headerTitle: selectedMeal.title,
+        headerTitle: mealTitle,
         headerRight: (
             <HeaderButtons HeaderButtonComponent={HeaderButton}>
                 <Item
