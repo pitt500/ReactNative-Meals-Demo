e08097588f32c1bbafb6855cfcb198a2c6ad93f9
pitt500 <pedro.rojaslo50@gmail.com>
2019-11-14 15:03:41 -0700

    Add store to provider with mealsReducer

--- rn-meals-app/App.js
+++ rn-meals-app/App.js
@@ -3,11 +3,20 @@ import { StyleSheet, Text, View } from 'react-native';
 import * as Font from 'expo-font';
 import { AppLoading } from 'expo';
 import { enableScreens } from 'react-native-screens';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
 
 import MealsNavigator from './navigation/MealsNavigator';
+import mealsReducer from './store/reducers/meals';
 
 //Use this to increase performance on larger applications
 enableScreens();
+
+const rootReducer = combineReducers({
+  meals: mealsReducer
+});
+
+const store = createStore(rootReducer);
 
 const fetchFonts = () => {
   return Font.loadAsync({
@@ -23,7 +32,7 @@ export default function App() {
     return <AppLoading startAsync={fetchFonts} onFinish={() => setFontLoaded(true)} />
   }
 
-  return <MealsNavigator />;
+  return <Provider store={store}><MealsNavigator /></Provider>;
 }
 
 const styles = StyleSheet.create({
